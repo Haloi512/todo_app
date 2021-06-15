9047a36a6995ba8c50c4c82c493c11cc9df2dbb7
Heart Bleed <hideonstorms@gmail.com>
2021-06-15 18:09:26 +0700

    add document for project

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -129,8 +129,8 @@ export default {
                 this.handleClose();
                 this.name = "";
                 this.description = "";
-                this.$store.commit("addData", res);
-                // this.$emit("handleRefesh");
+                // this.$store.commit("addData", res);
+                this.$emit("handleRefesh");
               }
             })
             .catch((err) => {
